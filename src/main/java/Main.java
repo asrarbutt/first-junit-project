8f0223db05da27dc35b51cccc8829f42a946f3de
asrarbutt <asrarbutt1986@gmail.com>
2022-06-30 08:52:06 +0200

    add method

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("my first jUnit-Test");
+    }
+
+    public static int add(int i){
+
+        return i*i;
+    }
+}
