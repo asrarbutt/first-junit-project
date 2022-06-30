2e049cf2c458c820f419afa24d468d0ce40cc904
asrarbutt <asrarbutt1986@gmail.com>
2022-06-30 11:51:39 +0200

    change test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("my first jUnit-Test");
+
+
     }
 
     public static int add(int i){
 
         return i+i;
+    }
+
+    public static boolean compare(int number1 , int number2){
+        return number1 >100 || number2 >100;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,7 +16,22 @@ class MainTest {
         int result= Main.add(i);
 
         //then
-        assertEquals(4, result);
+        Assertions.assertEquals(4, result);
+
+    }
+
+    @Test
+    void compare(){
+        // given
+        int number1=2000;
+        int number2=3;
+
+        //when
+        boolean result=Main.compare(number1, number2);
+        boolean expected=true;
+
+        //then
+        Assertions.assertEquals(expected, result);
 
     }
 
